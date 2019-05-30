3ceca2499aea22e3ce9d6247eadb337a18ef082c
Nataly Mazariegos <nataly_mazariegosp@hotmail.com>
2019-05-30 11:22:55 -0500

    icono y pantalla de inicio final

--- app/src/main/java/myapp/selectos/temas/chico/pet_friend/PantallaInicio.java
+++ app/src/main/java/myapp/selectos/temas/chico/pet_friend/PantallaInicio.java
@@ -2,8 +2,13 @@ package myapp.selectos.temas.chico.pet_friend;
 
 import android.content.Intent;
 import android.os.Handler;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.Slide;
+import android.transition.Transition;
+import android.view.Gravity;
+import android.view.animation.DecelerateInterpolator;
 
 public class PantallaInicio extends AppCompatActivity {
 
@@ -21,4 +26,6 @@ public class PantallaInicio extends AppCompatActivity {
             }
         },3000);
     }
+
+
 }
