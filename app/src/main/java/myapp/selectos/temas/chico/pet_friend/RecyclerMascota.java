151b8fbd0d36cc15b807ce88a86bc900ae29e88a
Nataly Mazariegos <nataly_mazariegosp@hotmail.com>
2019-05-31 08:14:00 -0500

    cardview actualizada

--- app/src/main/java/myapp/selectos/temas/chico/pet_friend/RecyclerMascota.java
+++ app/src/main/java/myapp/selectos/temas/chico/pet_friend/RecyclerMascota.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -14,14 +16,32 @@ public class RecyclerMascota extends AppCompatActivity {
     private RecyclerView rcMyPets;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
+
+    private TextView txtvtiempocomer;
+    private TextView txtvtiempobañar;
+    private TextView txtvfechavet;
+    private TextView txtvtiempovet;
+    private TextView txtvDirecvet;
+    private TextView txtvfechavac;
+    private TextView txtvtiempovac;
+    private TextView txtvDirecvac;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recycler_mascota);
 
         fabAddPet = findViewById(R.id.fabAddPet);
-
+        txtvtiempocomer=findViewById(R.id.txtvtiempocomer);
+        txtvtiempobañar=findViewById(R.id.txtvtiempobañar);
+        txtvfechavet=findViewById(R.id.txtvfechavet);
+        txtvtiempovet=findViewById(R.id.txtvtiempovet);
+        txtvDirecvet=findViewById(R.id.txtvDirecvet);
+        txtvfechavac=findViewById(R.id.txtvfechavac);
+        txtvtiempovac=findViewById(R.id.txtvtiempovac);
+        txtvDirecvac=findViewById(R.id.txtvDirecvac);
         ArrayList<MascotaDatos> datosMascotas = new ArrayList<>();
+
         datosMascotas.add(new MascotaDatos("Puchy", R.drawable.catdog));
         datosMascotas.add(new MascotaDatos("Puchy", R.drawable.catdog));
         datosMascotas.add(new MascotaDatos("Puchy", R.drawable.catdog));
@@ -39,6 +59,11 @@ public class RecyclerMascota extends AppCompatActivity {
 
         rcMyPets.setLayoutManager(mLayoutManager);
         rcMyPets.setAdapter(mAdapter);
+
+    }
+
+    public void onClickAdd(View v)
+    {
 
     }
 }
